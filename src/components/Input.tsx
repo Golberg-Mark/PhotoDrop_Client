b0d9d200fe9619b74c769944ece92e380f794e44
Mark_bitbucket <golbergmark@gmail.com>
2022-09-21 16:00:43 +0300

    Added input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,26 @@
+import React, { HTMLAttributes } from 'react';
+import styled from 'styled-components';
+
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input: React.FC<Props> = (props) => {
+  return (
+    <StyledInput {...props} />
+  );
+};
+
+const StyledInput = styled.input`
+  padding: 15px 13px;
+  width: 100%;
+  height: 40px;
+  border: 1px solid #EEE;
+  border-radius: 10px;
+  background-color: #EEE;
+  
+  :focus {
+    outline: none;
+    border: 1px solid #3300CC;
+  }
+`;
+
+export default Input;
