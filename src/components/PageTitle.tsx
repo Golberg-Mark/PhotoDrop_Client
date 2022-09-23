34c37989afd1925e214f385ef4b317ab121e07cc
Mark_bitbucket <golbergmark@gmail.com>
2022-09-23 13:34:05 +0300

    Added additional property for aligning text

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,24 +1,32 @@
 import React, { HTMLAttributes } from 'react';
 import styled from 'styled-components';
 
+type TextAlign = 'left' | 'center' | 'right';
+
 interface Props extends HTMLAttributes<HTMLHeadingElement>{
   fontSize?: number,
+  textAlign?: TextAlign,
   marginBottom?: number
 }
 
-const PageTitle: React.FC<Props> = ({ fontSize = 22, marginBottom = 14, children }) => {
+const PageTitle: React.FC<Props> = ({
+  fontSize = 22,
+  textAlign = 'center',
+  marginBottom = 14,
+  children
+}) => {
   return (
-    <StyledPageTitle fontSize={fontSize} marginBottom={marginBottom}>
+    <StyledPageTitle fontSize={fontSize} marginBottom={marginBottom} textAlign={textAlign}>
       {children}
     </StyledPageTitle>
   );
 };
 
-const StyledPageTitle = styled.h2<{ fontSize: number, marginBottom: number }>`
+const StyledPageTitle = styled.h2<{ fontSize: number, marginBottom: number, textAlign: TextAlign }>`
   margin-bottom: ${({ marginBottom }) => marginBottom}px;
   font-size: ${({ fontSize }) => fontSize}px;
   font-weight: 700;
-  text-align: center;
+  text-align: ${({ textAlign }) => textAlign};
 `;
 
 export default PageTitle;
