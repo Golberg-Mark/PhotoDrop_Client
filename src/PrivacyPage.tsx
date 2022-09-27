045d59ede8abeefb374a1b4d6b73609b9319d49a
Mark_bitbucket <golbergmark@gmail.com>
2022-09-27 22:54:56 +0300

    Added scroll to top when page was loaded at the first time

--- src/PrivacyPage.tsx
+++ src/PrivacyPage.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import PageTitle from '@/components/PageTitle';
 
 const PrivacyPage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <StyledPrivacyPage>
       <PageTitle>
